c9ae61333f9c95e0f57a0219387d1bf4c608efbb
LimRico <limyandi@gmail.com>
2019-01-10 21:18:36 +1100

    add missing Player class.

---
+++ Player.py
@@ -0,0 +1,10 @@
+class Player:
+    def __init__(self, id, symbol):
+        self.id = id
+        self.symbol = symbol
+
+    def get_id(self):
+        return self.id
+
+    def get_symbol(self):
+        return self.symbol
